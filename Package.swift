cf4ff2bc1dffb14adc27b1e5a09ac37e85424b99
Mark Anderson <mark@ionic.io>
2023-11-29 15:04:28 -0500

    fix: update urls to latest

--- Package.swift
+++ Package.swift
@@ -17,12 +17,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Capacitor",
-            url: "https://github.com/ionic-team/capacitor6-spm-test/releases/download/6.0.0-alpha.0/Capacitor.xcframework.zip",
+            url: "https://github.com/ionic-team/capacitor6-spm-test/releases/download/6.0.0-alpha.2/Capacitor.xcframework.zip",
             checksum: "473174e1217d66842c5b26f834f419d52457b129faf75fa566888dcb87ed9ac4"
         ),
         .binaryTarget(
             name: "Cordova",
-            url: "https://github.com/ionic-team/capacitor6-spm-test/releases/download/6.0.0-alpha.0/Cordova.xcframework.zip",
+            url: "https://github.com/ionic-team/capacitor6-spm-test/releases/download/6.0.0-alpha.2/Cordova.xcframework.zip",
             checksum: "93472649efb6a7a5cb4b2375b3d6a7f17499d8253c894fb11e770c516b2e74cf"
         )
     ]
