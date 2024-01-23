98315c71c9231eb3511b2562281a62eaad8dbd34
Mark Anderson <mark@ionic.io>
2024-01-23 12:54:36 -0500

    RC0 update

--- Package.swift
+++ Package.swift
@@ -17,13 +17,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Capacitor",
-            url: "https://github.com/ionic-team/capacitor6-spm-test/releases/download/6.0.0-beta.2/Capacitor.xcframework.zip",
-            checksum: "c29e7593151d6698f3de13208401be607acdd36b23516685e9e8f28bc758c9a4"
+            url: "https://github.com/ionic-team/capacitor6-spm-test/releases/download/6.0.0-rc.0/Capacitor.xcframework.zip",
+            checksum: "ddc07c89b8f8c0fb930f678be1cdc1ed632ba51e8f7f4a02369b6f5368ff14cd"
         ),
         .binaryTarget(
             name: "Cordova",
-            url: "https://github.com/ionic-team/capacitor6-spm-test/releases/download/6.0.0-beta.2/Cordova.xcframework.zip",
-            checksum: "fab702c6ad86e150ff7baca549ca926bdf533eb3002046b6b27aaa17ce026e3a"
+            url: "https://github.com/ionic-team/capacitor6-spm-test/releases/download/6.0.0-rc.0/Cordova.xcframework.zip",
+            checksum: "e83997d0dc4781b09a6ea5206721a7375faeaa11cce326db3898e7df3a291bbb"
         )
     ]
 )
